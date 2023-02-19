9e6d28e7cf5e8eafb54fe30a1e80f6344f76e3cb
shuhai1996 <hgyuhang@qq.com>
2023-02-19 14:01:56 +0800

    update

---
+++ go.mod
@@ -0,0 +1,24 @@
+module go-wechat
+
+go 1.19
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/websocket v1.4.1
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.0
+	github.com/satori/go.uuid v1.2.0
+	github.com/ugjka/messenger v1.1.3
+	google.golang.org/grpc v1.53.0
+	google.golang.org/protobuf v1.28.1
+	nhooyr.io/websocket v1.8.7
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/klauspost/compress v1.10.3 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+)
---
+++ middleware/jwt.go
@@ -0,0 +1,57 @@
+package middleware
+
+import "C"
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/satori/go.uuid"
+)
+
+// CustomClaims 自定义声明
+type CustomClaims struct {
+	UUID     uuid.UUID `json:"uuid"`
+	ID       int `json:"id"`
+	Username string `json:"username"`
+	NickName string `json:"nick_name"`
+	jwt.StandardClaims
+}
+
+type User struct {
+	ID       int `json:"id"`
+	Username string `json:"username"`
+	Nickname string `json:"nickname"`
+}
+
+var secret = "###########"
+
+
+// JwtAuth 后台鉴权中间件
+func JwtAuth(jwtS string) (user *User, err error){
+
+	if jwtS == "" {
+		err = fmt.Errorf("empty token")
+		return
+	}
+
+	// 解析Token
+	claims := &CustomClaims{}
+	token, err := jwt.ParseWithClaims(jwtS, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+
+	// 验证Token是否有效
+	if !token.Valid || err!=nil{
+		return
+	}
+
+	if err != nil {
+		return
+	}
+	fmt.Println(claims)
+
+	return &User{
+		Username: claims.Username,
+		Nickname: claims.NickName,
+		ID: claims.ID,
+	}, nil
+}
